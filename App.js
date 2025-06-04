43e8c4bcdd3b171378230aabdfa7d3fb204997a8
Ezekiel-faith <ezekielfaith20002gmail.com>
2025-06-04 13:30:40 +0100

    added image background

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />;
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImg}
+      >
+        <StartGameScreen />;
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +20,9 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+
+  backgroundImg: {
+    opacity: 0.15,
   },
 });
